509a328f02e81990f36a033e6dd945e4e3a3b8f1
Lilles420 <115455132+Lilles420@users.noreply.github.com>
2023-01-17 14:27:58 +0200

    Add files via upload

---
+++ yls7.ps1
@@ -0,0 +1,27 @@
+﻿scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$csvinp = read-host "Mis on sinu CSV faili nimetus"
+
+if ($csvinp -notmatch "$_.csv")
+{
+    $csvinp = read-host "Seda faili pole voimalik selles skriptis kasutada. Palun pange teine fail"
+}
+else
+{
+    write-host "$csvinp on sobiva faililaiendiga."
+    $emailinp = read-host "mis on sinu domeen (naiteks '@hkhk.edu.ee')"
+}
+
+$emails = import-csv $dir\$csvinp -header id,first_name,last_name,email,gender,ip_address  | Select-Object  id,first_name,last_name,email,gender,ip_address -skip 1
+new-item $dir\emailid.txt
+
+foreach($nimi in $emails){
+
+    $enimi = $nimi.first_name
+    $lnimi = $nimi.last_name
+
+    $julla = $enimi.ToLower()+$vnimi.ToLower()+$emailinp
+    $julla >> $dir/emailid.txt
+
+}
